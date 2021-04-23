1b678e377ec30a829d4b402f203ffd0551da74c1
winglim <643089849@qq.com>
2021-04-23 12:25:46 +0800

    feat: add gitee webhook

---
+++ gitee.go
@@ -0,0 +1,78 @@
+package caddy_webhook
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/go-git/go-git/v5/plumbing"
+	"io/ioutil"
+	"net/http"
+)
+
+type Gitee struct {
+}
+
+type giteePush struct {
+	Ref string `json:"ref"`
+}
+
+func (g Gitee) Handle(r *http.Request, repo *Repo) (int, error) {
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return http.StatusBadRequest, err
+	}
+
+	err = g.handleToken(r, repo.Secret)
+	if err != nil {
+		return http.StatusBadRequest, err
+	}
+
+	event := r.Header.Get("X-Gitee-Event")
+	if event == "" {
+		return http.StatusBadRequest, fmt.Errorf("header 'X-Gitee-Event' missing")
+	}
+
+	switch event {
+	case "Push hook":
+		err = g.handlePush(body, repo)
+		if err != nil {
+			return http.StatusBadRequest, err
+		}
+	default:
+		return http.StatusBadRequest, nil
+	}
+
+	return http.StatusOK, nil
+}
+
+func (g Gitee) handleToken(r *http.Request, secret string) error {
+	token := r.Header.Get("X-Gitee-Token")
+	if token != "" {
+		if secret == "" {
+			return fmt.Errorf("empty webhook secret")
+		} else {
+			if token != secret {
+				return fmt.Errorf("inavlid token")
+			}
+		}
+	}
+
+	return nil
+}
+
+func (g Gitee) handlePush(body []byte, repo *Repo) error {
+	var push giteePush
+
+	err := json.Unmarshal(body, &push)
+	if err != nil {
+		return err
+	}
+
+	refName := plumbing.ReferenceName(push.Ref)
+	if refName.IsBranch() {
+		if refName != repo.refName {
+			return fmt.Errorf("event: push to branch %s", refName)
+		}
+	}
+	return nil
+}
