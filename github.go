8c938d185177d1f758032e9385d83db6b433a11c
winglim <643089849@qq.com>
2021-04-23 11:33:25 +0800

    feat: split out event handle function

--- github.go
+++ github.go
@@ -6,6 +6,7 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"github.com/go-git/go-git/v5/plumbing"
 	"io/ioutil"
 	"net/http"
 )
@@ -13,36 +14,29 @@ import (
 type Github struct {
 }
 
-type pushBody struct {
+type ghPush struct {
 	Ref string `json:"ref"`
 }
 
-func (Github) Handle(req *http.Request, hc *HookConf) (int, error) {
-	if err := ValidateRequest(req); err != nil {
+type ghRelease struct {
+	Action  string `json:"action"`
+	Release struct {
+		TagName string `json:"tag_name"`
+	} `json:"release"`
+}
+
+func (g Github) Handle(r *http.Request, repo *Repo) (int, error) {
+	if err := ValidateRequest(r); err != nil {
 		return http.StatusBadRequest, err
 	}
 
-	body, err := ioutil.ReadAll(req.Body)
+	body, err := ioutil.ReadAll(r.Body)
+	err = g.handleSignature(r, body, repo.Secret)
 	if err != nil {
-		return http.StatusRequestTimeout, err
+		return http.StatusBadRequest, err
 	}
 
-	signature := req.Header.Get("X-Hub-Signature")
-	if signature != "" {
-		if hc.Secret == "" {
-			return http.StatusBadRequest, fmt.Errorf("empty webhook secret")
-		}
-
-		mac := hmac.New(sha1.New, []byte(hc.Secret))
-		mac.Write(body)
-		expectedMac := hex.EncodeToString(mac.Sum(nil))
-
-		if signature[5:] != expectedMac {
-			return http.StatusBadRequest, fmt.Errorf("inavlid signature")
-		}
-	}
-
-	event := req.Header.Get("X-Github-Event")
+	event := r.Header.Get("X-Github-Event")
 	if event == "" {
 		return http.StatusBadRequest, fmt.Errorf("header 'X-Github-Event' missing")
 	}
@@ -50,9 +44,12 @@ func (Github) Handle(req *http.Request, hc *HookConf) (int, error) {
 	switch event {
 	case "ping":
 	case "push":
-		var rBody pushBody
-
-		err = json.Unmarshal(body, &rBody)
+		err = g.handlePush(body, repo)
+		if err != nil {
+			return http.StatusBadRequest, err
+		}
+	case "release":
+		err = g.handleRelease(body, repo)
 		if err != nil {
 			return http.StatusBadRequest, err
 		}
@@ -61,4 +58,54 @@ func (Github) Handle(req *http.Request, hc *HookConf) (int, error) {
 	}
 
 	return http.StatusOK, nil
+}
+
+func (g Github) handleSignature(r *http.Request, body []byte, secret string) error {
+	signature := r.Header.Get("X-Hub-Signature")
+	if signature != "" {
+		if secret == "" {
+			return fmt.Errorf("empty webhook secret")
+		} else {
+			mac := hmac.New(sha1.New, []byte(secret))
+			mac.Write(body)
+			expectedMac := hex.EncodeToString(mac.Sum(nil))
+
+			if signature[5:] != expectedMac {
+				return fmt.Errorf("inavlid signature")
+			}
+		}
+	}
+
+	return nil
+}
+
+func (g Github) handlePush(body []byte, repo *Repo) error {
+	var push ghPush
+
+	err := json.Unmarshal(body, &push)
+	if err != nil {
+		return err
+	}
+
+	refName := plumbing.ReferenceName(push.Ref)
+	if refName.IsBranch() {
+		if refName != repo.refName {
+			return fmt.Errorf("event: push to branch %s", refName)
+		}
+	}
+	return nil
+}
+
+func (g Github) handleRelease(body []byte, repo *Repo) error {
+	var release ghRelease
+
+	err := json.Unmarshal(body, &release)
+	if err != nil {
+		return err
+	}
+	if release.Release.TagName == "" {
+		return fmt.Errorf("invalid (empty) tag name")
+	}
+
+	return nil
 }
