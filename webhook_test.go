895de9a469bcb0d5aadf32d6973e17e363dcad11
winglim <643089849@qq.com>
2021-05-07 11:13:30 +0800

    test: isEmptyOrGit function

--- webhook_test.go
+++ webhook_test.go
@@ -1,7 +1,10 @@
 package caddy_webhook
 
 import (
+	"fmt"
 	"github.com/alecthomas/assert"
+	"github.com/go-git/go-git/v5"
+	"os"
 	"testing"
 )
 
@@ -25,5 +28,56 @@ func TestGetRepoNameFromURL(t *testing.T) {
 			assert.Nil(t, err)
 			assert.Equal(t, tc.name, name)
 		}
+	}
+}
+
+func TestIsEmptyOrGit(t *testing.T) {
+	testCases := []struct {
+		path     string
+		expected bool
+	}{
+		{"test/not_exist", true},
+		{"test/empty", true},
+		{"test/not_empty", false},
+		{"test/file", false},
+		{"test/dir_git", true},
+	}
+
+	err := os.Mkdir("test", 0666)
+	defer func() {
+		err := os.RemoveAll("test")
+		assert.Nil(t, err)
+	}()
+	assert.Nil(t, err)
+
+	err = os.Mkdir("test/empty", 0666)
+	assert.Nil(t, err)
+
+	err = os.Mkdir("test/not_empty", 0666)
+	assert.Nil(t, err)
+	file1, err := os.Create("test/not_empty/test.txt")
+	defer func(file1 *os.File) {
+		err := file1.Close()
+		assert.Nil(t, err)
+	}(file1)
+	assert.Nil(t, err)
+
+	file2, err := os.Create("test/file")
+	defer func(file2 *os.File) {
+		err := file2.Close()
+		assert.Nil(t, err)
+	}(file2)
+	assert.Nil(t, err)
+
+	err = os.Mkdir("test/dir", 0666)
+	assert.Nil(t, err)
+
+	_, err = git.PlainInit("test/dir_git", false)
+	assert.Nil(t, err)
+
+	for i, tc := range testCases {
+		actual := isEmptyOrGit(tc.path, nil)
+
+		assert.Equal(t, tc.expected, actual, fmt.Sprintf("case %d", i))
 	}
 }
